abb888de2802d3535bdd9a9064d969871e746a29
Andres_Github <81278332+Slushi-Github@users.noreply.github.com>
2025-09-27 12:33:48 -0400

    add new things

---
+++ switchLib/arm/Counter.hx
@@ -0,0 +1,17 @@
+package switchLib.arm;
+
+#if !HX_NX
+#error "This file should only be compiled for the Nintendo Switch target"
+#end
+
+import cpp.*;
+
+@:buildXml('<include name="${haxelib:hx_libnx}/include.xml"/>')
+@:include("switch/arm/counter.h")
+extern class Counter {
+    @:native("armGetSystemTick")
+    extern public static function armGetSystemTick():UInt64;
+
+    @:native("armTicksToNs")
+    extern public static function armTicksToNs(ticks:UInt64):UInt64;
+}
--- switchLib/services/Applet.hx
+++ switchLib/services/Applet.hx
@@ -8,7 +8,26 @@ import cpp.*;
 
 @:buildXml('<include name="${haxelib:hx_libnx}/include.xml"/>')
 @:include("switch.h")
+@:native("AppletType")
+extern enum abstract AppletType(Int) to Int {
+    @:native("AppletType_None") var AppletType_None;
+    @:native("AppletType_Default") var AppletType_Default;
+    @:native("AppletType_Application") var AppletType_Application;
+    @:native("AppletType_SystemApplet") var AppletType_SystemApplet;
+    @:native("AppletType_LibraryApplet") var AppletType_LibraryApplet;
+    @:native("AppletType_OverlayApplet") var AppletType_OverlayApplet;
+    @:native("AppletType_SystemApplication") var AppletType_SystemApplication;
+}
+
+@:buildXml('<include name="${haxelib:hx_libnx}/include.xml"/>')
+@:include("switch.h")
 extern class Applet {
     @:native("appletMainLoop")
     extern public static function appletMainLoop():Bool;
+
+    @:native("appletGetAppletType")
+    extern public static function appletGetAppletType():AppletType;
+
+    @:native("appletExit")
+    extern public static function appletExit():Void;
 }
