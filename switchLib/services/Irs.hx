d69035eefc93ea37417d6156cc222cdedb48f95a
Andres_Github <81278332+Slushi-Github@users.noreply.github.com>
2026-06-13 11:09:10 -0400

    Added new functions to Irs

--- switchLib/services/Irs.hx
+++ switchLib/services/Irs.hx
@@ -4,7 +4,34 @@ package switchLib.services;
 #error "This file should only be compiled for the Nintendo Switch target"
 #end
 
-import switchLib.services.Hid;
+import cpp.UInt8;
+import cpp.UInt32;
+import cpp.UInt64;
+import cpp.Int32;
+import cpp.SizeT;
+import cpp.Pointer;
+import cpp.ConstPointer;
+import cpp.RawPointer;
+import switchLib.Types.ResultType;
+
+@:buildXml('<include name="${haxelib:hx_libnx}/include.xml"/>')
+@:include("switch.h")
+extern class IrsImageTransferProcessorFormat {
+    @:native("IrsImageTransferProcessorFormat_320x240")
+    public static var IrsImageTransferProcessorFormat_320x240:UInt32;
+
+    @:native("IrsImageTransferProcessorFormat_160x120")
+    public static var IrsImageTransferProcessorFormat_160x120:UInt32;
+
+    @:native("IrsImageTransferProcessorFormat_80x60")
+    public static var IrsImageTransferProcessorFormat_80x60:UInt32;
+
+    @:native("IrsImageTransferProcessorFormat_40x30")
+    public static var IrsImageTransferProcessorFormat_40x30:UInt32;
+
+    @:native("IrsImageTransferProcessorFormat_20x15")
+    public static var IrsImageTransferProcessorFormat_20x15:UInt32;
+}
 
 @:buildXml('<include name="${haxelib:hx_libnx}/include.xml"/>')
 @:include("switch.h")
@@ -12,13 +39,13 @@ import switchLib.services.Hid;
 @:structAccess
 extern class IrsIrCameraHandle {
     @:native("player_number")
-    extern public var player_number:UInt8;
+    public var player_number:UInt8;
 
     @:native("device_type")
-    extern public var device_type:UInt8;
+    public var device_type:UInt8;
 
     @:native("reserved")
-    extern public var reserved:RawPointer<UInt8>;
+    public var reserved:RawPointer<UInt8>;
 
     @:haxe.warning("-WExternWithExpr")
     public function new() {}
@@ -30,22 +57,22 @@ extern class IrsIrCameraHandle {
 @:structAccess
 extern class IrsImageTransferProcessorConfig {
     @:native("exposure_time")
-    extern public var exposure_time:UInt64;
+    public var exposure_time:UInt64;
 
     @:native("light_target")
-    extern public var light_target:UInt32;
+    public var light_target:UInt32;
 
     @:native("gain")
-    extern public var gain:UInt32;
+    public var gain:UInt32;
 
     @:native("is_negative_image_used")
-    extern public var is_negative_image_used:UInt8;
+    public var is_negative_image_used:UInt8;
 
     @:native("reserved")
-    extern public var reserved:RawPointer<UInt8>;
+    public var reserved:RawPointer<UInt8>;
 
     @:native("format")
-    extern public var format:UInt32;
+    public var format:UInt32;
 
     @:haxe.warning("-WExternWithExpr")
     public function new() {}
@@ -57,13 +84,13 @@ extern class IrsImageTransferProcessorConfig {
 @:structAccess
 extern class IrsImageTransferProcessorState {
     @:native("sampling_number")
-    extern public var sampling_number:UInt64;
+    public var sampling_number:UInt64;
 
     @:native("ambient_noise_level")
-    extern public var ambient_noise_level:UInt32;
+    public var ambient_noise_level:UInt32;
 
     @:native("reserved")
-    extern public var reserved:RawPointer<UInt8>;   
+    public var reserved:RawPointer<UInt8>;
 
     @:haxe.warning("-WExternWithExpr")
     public function new() {}
@@ -72,22 +99,30 @@ extern class IrsImageTransferProcessorState {
 @:buildXml('<include name="${haxelib:hx_libnx}/include.xml"/>')
 @:include("switch.h")
 extern class Irs {
-
     @:native("irsInitialize")
-    extern public static function irsInitialize():ResultType;
+    public static function irsInitialize():ResultType;
+
+    @:native("irsExit")
+    public static function irsExit():Void;
 
     @:native("irsGetIrCameraHandle")
-    extern public static function irsGetIrCameraHandle(handle:Pointer<IrsIrCameraHandle>, id:Int32):ResultType;
+    public static function irsGetIrCameraHandle(handle:Pointer<IrsIrCameraHandle>, id:Int32):ResultType;
 
     @:native("irsGetDefaultImageTransferProcessorConfig")
-    extern public static function irsGetDefaultImageTransferProcessorConfig(config:Pointer<IrsImageTransferProcessorConfig>):Void;
+    public static function irsGetDefaultImageTransferProcessorConfig(config:Pointer<IrsImageTransferProcessorConfig>):Void;
 
     @:native("irsRunImageTransferProcessor")
-    extern public static function irsStartImageTransferProcessor(handle:IrsIrCameraHandle, config:ConstPointer<IrsImageTransferProcessorConfig>, size:SizeT):ResultType;
+    public static function irsRunImageTransferProcessor(handle:IrsIrCameraHandle, config:ConstPointer<IrsImageTransferProcessorConfig>, size:SizeT):ResultType;
 
     @:native("irsGetImageTransferProcessorState")
-    extern public static function irsGetImageTransferProcessorState(handle:IrsIrCameraHandle, buffer:Pointer<cpp.Void>, size:SizeT, state:Pointer<IrsImageTransferProcessorState>):ResultType;
+    public static function irsGetImageTransferProcessorState(handle:IrsIrCameraHandle, buffer:Pointer<cpp.Void>, size:SizeT, state:Pointer<IrsImageTransferProcessorState>):ResultType;
+
+    @:native("irsStopImageProcessor")
+    public static function irsStopImageProcessor(handle:IrsIrCameraHandle):ResultType;
 
     @:native("irsStopImageProcessorAsync")
-    extern public static function irsStopImageProcessorAsync(handle:IrsIrCameraHandle):ResultType;
+    public static function irsStopImageProcessorAsync(handle:IrsIrCameraHandle):ResultType;
+
+    @:native("irsCheckFirmwareUpdateNecessity")
+    public static function irsCheckFirmwareUpdateNecessity(handle:IrsIrCameraHandle, out:Pointer<Bool>):ResultType;
 }
